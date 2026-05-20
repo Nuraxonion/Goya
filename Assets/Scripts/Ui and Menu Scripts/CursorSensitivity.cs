6bafeb06f189cbf7445a6dec6596e355d3aaad99
Mitrinho22 <nmitri22@gmail.com>
2026-05-20 13:38:21 +0200

    Changes to the fakecursor to check sensitivity

--- Assets/Scripts/Ui and Menu Scripts/CursorSensitivity.cs
+++ Assets/Scripts/Ui and Menu Scripts/CursorSensitivity.cs
@@ -1,19 +1,95 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class CursorSensitivity : MonoBehaviour
 {
+    public RectTransform cursorTransform;
+
+    private Vector2 virtualMousePosition;
+
+    private GameObject currentObject;
+
+    private PointerEventData pointerData;
+
     void Start()
     {
-        Cursor.visible = true;
+        Cursor.visible = false;
+
+        virtualMousePosition = Input.mousePosition;
+
+        cursorTransform.position = virtualMousePosition;
     }
 
     void Update()
     {
         float sensitivity = MenuManager.mouseSensitivity;
 
-        Vector3 mousePos = Input.mousePosition;
+        float mouseX = Input.GetAxis("Mouse X") * sensitivity * 25f;
+        float mouseY = Input.GetAxis("Mouse Y") * sensitivity * 25f;
 
-        mousePos.x *= sensitivity;
-        mousePos.y *= sensitivity;
+        virtualMousePosition += new Vector2(mouseX, mouseY);
+
+        virtualMousePosition.x = Mathf.Clamp(virtualMousePosition.x, 0, Screen.width);
+        virtualMousePosition.y = Mathf.Clamp(virtualMousePosition.y, 0, Screen.height);
+
+        cursorTransform.position = virtualMousePosition;
+
+        HandleUI();
+    }
+
+    void HandleUI()
+    {
+        pointerData = new PointerEventData(EventSystem.current);
+
+        pointerData.position = virtualMousePosition;
+
+        List<RaycastResult> results = new List<RaycastResult>();
+
+        EventSystem.current.RaycastAll(pointerData, results);
+
+        if (results.Count > 0)
+        {
+            currentObject = results[0].gameObject;
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                ExecuteEvents.Execute(
+                    currentObject,
+                    pointerData,
+                    ExecuteEvents.pointerDownHandler
+                );
+            }
+
+            if (Input.GetMouseButtonUp(0))
+            {
+                ExecuteEvents.Execute(
+                    currentObject,
+                    pointerData,
+                    ExecuteEvents.pointerUpHandler
+                );
+
+                ExecuteEvents.Execute(
+                    currentObject,
+                    pointerData,
+                    ExecuteEvents.pointerClickHandler
+                );
+            }
+
+            if (Input.GetMouseButton(0))
+            {
+                ExecuteEvents.Execute(
+                    currentObject,
+                    pointerData,
+                    ExecuteEvents.beginDragHandler
+                );
+
+                ExecuteEvents.Execute(
+                    currentObject,
+                    pointerData,
+                    ExecuteEvents.dragHandler
+                );
+            }
+        }
     }
 }
