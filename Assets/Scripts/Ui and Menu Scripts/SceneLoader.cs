3a29a6a12a1fe5cb8075df351b8d274816b84884
Mitrinho22 <nmitri22@gmail.com>
2026-05-25 10:18:27 -0500

    SceneLoader/ Playbutton functionality

---
+++ Assets/Scripts/Ui and Menu Scripts/SceneLoader.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadNextScene()
+    {
+        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+}
